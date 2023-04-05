844768f70b7b748dfa9ac5063528886f68077fa6
KenStarry <starrycodes@gmail.com>
2023-04-05 20:54:48 +0300

    Finished login screen

--- app/src/main/java/com/kenstarry/snacky/feature_authentication/login/presentation/LoginScreen.kt
+++ app/src/main/java/com/kenstarry/snacky/feature_authentication/login/presentation/LoginScreen.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AlternateEmail
+import androidx.compose.material.icons.outlined.Key
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -15,11 +18,14 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.kenstarry.snacky.R
+import com.kenstarry.snacky.core.presentation.components.CustomTextField
 import com.kenstarry.snacky.ui.custom.spacing
 
 @Composable
@@ -35,7 +41,7 @@ fun LoginScreen(
             .background(MaterialTheme.colorScheme.onPrimary)
             .verticalScroll(loginScrollState)
             .padding(MaterialTheme.spacing.medium),
-        verticalArrangement = Arrangement.spacedBy(24.dp),
+        verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.large),
         horizontalAlignment = Alignment.Start
     ) {
 
@@ -59,19 +65,27 @@ fun LoginScreen(
                 },
                 style = MaterialTheme.typography.titleLarge,
                 maxLines = 1,
-                textAlign = TextAlign.Center
+                textAlign = TextAlign.Center,
+                color = MaterialTheme.colorScheme.onSecondaryContainer
             )
 
         }
 
         //  svg image
-        Image(
-            painter = painterResource(id = R.drawable.undraw_ice_cream_s_2_rh),
-            contentDescription = "Snacky svg image",
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
-                .height(200.dp)
-        )
+                .wrapContentHeight(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.undraw_ice_cream_s_2_rh),
+                contentDescription = "Snacky svg image",
+                modifier = Modifier
+                    .fillMaxWidth(0.9f)
+                    .height(200.dp)
+            )
+        }
 
         //  login text
         Text(
@@ -80,11 +94,41 @@ fun LoginScreen(
             color = MaterialTheme.colorScheme.onSecondaryContainer.copy(alpha = 0.8f)
         )
 
-        //  login text field
-        TextField(
-            value = "",
-            onValueChange = {}
-        )
+        //  email text field
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.spacedBy(MaterialTheme.spacing.medium)
+        ) {
+
+            //  Email Address
+            CustomTextField(
+                startIcon = Icons.Outlined.AlternateEmail,
+                endIcon = null,
+                placeholder = "Email Address",
+                imeAction = ImeAction.Done,
+                keyboardType = KeyboardType.Email,
+                primaryColor = MaterialTheme.colorScheme.primary,
+                tertiaryColor = MaterialTheme.colorScheme.tertiary,
+                onInput = {}
+            )
+
+            //  Password
+            CustomTextField(
+                startIcon = Icons.Outlined.Key,
+                endIcon = null,
+                placeholder = "Password",
+                imeAction = ImeAction.Done,
+                keyboardType = KeyboardType.Password,
+                primaryColor = MaterialTheme.colorScheme.primary,
+                tertiaryColor = MaterialTheme.colorScheme.tertiary,
+                isPassword = true,
+                onInput = {}
+            )
+
+        }
     }
 
 }
